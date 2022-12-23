9ab6a14995b2ecad636d46b67d423f367c1b0c6a
George S <reneehe@George-Sem-2.local>
2022-12-22 20:17:18 -0800

    add logic for random password generation

--- Develop/script.js
+++ Develop/script.js
@@ -14,33 +14,56 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var length = prompt("Enter password length as a number: Between 8 and 128");
+  var generatedPassword = "";
+  var length = Number(prompt("Enter password length as a number: Between 8 and 128"));
+
+  //handle proper length range required
+  while(length < 8 || length > 128) {
+    var length = Number(prompt("Previous entry is invalid, please enter a number between 8 and 128 inclusive"));
+  }
+
   var lowercase = confirm("Click Ok if you want lowercase letters, cancel if not");
   var uppercase = confirm("Click Ok if you want UPPERCASE letters, cancel if not");
   var numeric = confirm("Click Ok if you want digits, cancel if not");
   var specialCharacters = confirm("Click Ok if you want special characters, cancel if not");
 
-console.log(length);
-console.log(lowercase);
-console.log(uppercase);
-console.log(numeric);
-console.log(specialCharacters);
+  //handle case if all false
+  while((lowercase || uppercase || numeric || specialCharacters) == false) {
+    alert("Please select at least one requirement other than length");
+    lowercase = confirm("Click Ok if you want lowercase letters, cancel if not");
+    uppercase = confirm("Click Ok if you want UPPERCASE letters, cancel if not");
+    numeric = confirm("Click Ok if you want digits, cancel if not");
+    specialCharacters = confirm("Click Ok if you want special characters, cancel if not");
+  }
 
+  var possibileDigits = "0123456789";
+  var possibleLowerAlphas = "abcdefghijklmnopqrstuvwxyz";
+  var possibleUpperAlphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var possibleSpecialChars = "~!@#$%^&*()_+-=[]{}|;:,./<>?";
+  var totalPossibleCharacters = "";
 
+  if(lowercase == true) {
+    totalPossibleCharacters += possibleLowerAlphas;
+  }
 
-  return "HELLOOOOO";
+  if(uppercase == true) {
+    totalPossibleCharacters += possibleUpperAlphas;
+  }
+
+  if(numeric == true){
+    totalPossibleCharacters += possibileDigits;
+  }
+
+  if(specialCharacters == true) {
+    totalPossibleCharacters += possibleSpecialChars;
+  }
+
+  for(var i = 0; i <= length; i++){
+    var random = Math.floor(Math.random() * totalPossibleCharacters.length);
+    generatedPassword += totalPossibleCharacters.substring(random, random + 1);
+  }
+
+  return generatedPassword;
 }
-
-console.log();
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-//criteria
-//length 8 - 128 chars
-//lowercase, uppercase, numeric, special chars
-// 1 prompt for each
-//at least one character type should be selected
-//generate password after all prompts answered
-//password then displayed in an alert or on the page itself
